319fcff4bee84cf7139b1fdd9a91adace2b43620
Saad Amir <59224810+saadamirpk@users.noreply.github.com>
2022-05-17 03:59:54 +0500

    Delete Transaction

--- src/Components/Transaction.tsx
+++ src/Components/Transaction.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import ic from "../del-ic.png";
+import { GlobalContext } from "../Context/GlobalState";
 
 export default function Transaction({ transaction }: any) {
   const sign = transaction.amount < 0 ? "-" : "+";
+
+  const { deleteTransaction } = useContext(GlobalContext);
 
   function getAmount(num: number) {
     return num.toLocaleString();
@@ -10,6 +14,9 @@ export default function Transaction({ transaction }: any) {
   return (
     <div className={(sign == "-" ? "minus" : "plus") + " transaction "}>
       {transaction.text}
+      <div onClick={() => deleteTransaction(transaction.id)} className="del">
+        <img className="ic" src={ic} />
+      </div>
       <span className="amount">
         {sign} {getAmount(Math.abs(transaction.amount))}
       </span>
